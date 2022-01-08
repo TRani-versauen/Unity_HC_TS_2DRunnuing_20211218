411dd82b1d05dc32dea8fdff989426cf03e74fb9
KID <50666207+KID421@users.noreply.github.com>
2022-01-08 19:34:11 +0800

    Update 建立汽車類別以及認識欄位語法

---
+++ 2DRunning_20211218/Assets/Scripts/Car.cs
@@ -0,0 +1,15 @@
+using UnityEngine;  // 引用 Unity 引擎空間，讓我們可以使用裡面的 API (倉庫)
+
+// 類別 類別名稱 (必須與檔案名稱完全一致，大小寫也是)
+public class Car : MonoBehaviour
+{
+    // 此類別的內容
+
+    #region 欄位 Field
+    // ※ 欄位語法
+    // 資料類型 欄位名稱 結尾
+    int cc;
+    #endregion
+}
+
+// 非汽車類別資料
