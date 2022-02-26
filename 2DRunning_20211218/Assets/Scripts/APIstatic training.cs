f4dd4eb482ac129cbbed1c0184867731642b347d
TRani-versauen <96644752+TRani-versauen@users.noreply.github.com>
2022-02-26 17:08:06 +0800

    5:08

--- 2DRunning_20211218/Assets/Scripts/APIstatic training.cs
+++ 2DRunning_20211218/Assets/Scripts/APIstatic training.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 /// <summary>
 /// 自行練習API 練習內容 老師給的目標
-/// 搜尋unity API ...  得知用法
+/// 搜尋unity API ...  得知用法 https://docs.unity3d.com/ScriptReference/Application.OpenURL.html
 /// 內容網址 https://paper.dropbox.com/doc/C--Bcqr9juySGNcnwbnJU5KkZVIAg-rEKAPFSoMNq3GOxEx45vY
 /// </summary>
 public class APIstatictraining : MonoBehaviour
