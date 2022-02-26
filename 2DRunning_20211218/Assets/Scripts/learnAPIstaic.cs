7721bc0b235334c48e1d3e18a1d3eaf12c26c40d
TRani-versauen <96644752+TRani-versauen@users.noreply.github.com>
2022-02-26 14:42:06 +0800

    2:41

--- 2DRunning_20211218/Assets/Scripts/learnAPIstaic.cs
+++ 2DRunning_20211218/Assets/Scripts/learnAPIstaic.cs
@@ -2,7 +2,22 @@
 using UnityEngine;
 
 /// <summary>
-/// 學習API
+/// 學習靜態API
+/// 靜態 static
+/// 非實體物件，不存在遊戲場景內的物件
 /// </summary>
 public class learnAPIstaic : MonoBehaviour
+{
+    private void Start()
+    {
+        //靜態屬性語法
+        //取得 Get
+        //類別名稱.靜態屬性名稱
+        print("圓周率" + Mathf.PI);
+        print("無限大" + Mathf.Infinity);
+
+        //存放 Set
+    }
+}
+    
 
