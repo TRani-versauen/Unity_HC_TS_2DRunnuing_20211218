ff432b8770e71b8c90c0065956f7120203f6fb20
TRani-versauen <96644752+TRani-versauen@users.noreply.github.com>
2022-03-12 16:25:50 +0800

    4:25

---
+++ 2DRunning_20211218/Assets/Scripts/Prop.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Prop : MonoBehaviour
+{
+    [Header("¤À¼Æ"), Range(0, 1000)]
+    public int Score;
+}
--- 2DRunning_20211218/Assets/Scripts/gameManger.cs
+++ 2DRunning_20211218/Assets/Scripts/gameManger.cs
@@ -11,6 +11,9 @@ public class gameManger : MonoBehaviour
     public Text textScore;
     [Header("玩家血量"), Range(0, 5000)]
     public float hp = 100;
+    [Header("道具")]
+    public string tagProp = "道具";
+
 
     private int score;
     private float hpMax;
@@ -18,11 +21,49 @@ public class gameManger : MonoBehaviour
     private void Update()
     {
         UpdateTimeUI();
+        UpdateHpUI();
     }
 
+    private void Start()
+    {
+        hpMax = hp;              //遊戲開始時的血量
+    }
+
+    /// <summary>
+    /// 更新時間介面
+    /// </summary>
     private void UpdateTimeUI()
     {
-        print("當前場景時間" + Time.timeSinceLevelLoad);
+        //print("當前場景時間" + Time.timeSinceLevelLoad);
+        //ToString("F1")數字表示小數點後幾位數
+        textTime.text = "時間:" + Time.timeSinceLevelLoad.ToString("F1");
     }
+
+    /// <summary>
+    /// 更新血量介面
+    /// </summary>
+    private void UpdateHpUI()
+    {
+        hp -= Time.deltaTime;
+        imgHp.fillAmount = hp / hpMax;
+    }
+
+    private void AddScoreAndUpdateUI()
+    {
+        score += 100;
+        textScore.text = "分數" + score;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //print("玩家碰到:" + collision.name);
+        if (collision.tag == tagProp)
+        {
+            AddScoreAndUpdateUI();
+            Destroy(collision.gameObject);
+        }
+    }
+
+    
 }
 
